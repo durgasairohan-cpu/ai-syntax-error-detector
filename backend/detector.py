9fb4a4ea70d007b5123c683eae2b317ce05b5af1
Durga sai Rohan <durgasairohan@gmail.com>
2026-05-17 18:46:55 +0530

    Update detector.py

--- backend/detector.py
+++ backend/detector.py
@@ -1,14 +1,20 @@
 import ast
+import subprocess
+import tempfile
+import os
 
 
+# Python Detection
 def detect_python_errors(code: str):
+
     errors = []
 
     try:
         ast.parse(code)
+
     except SyntaxError as e:
         errors.append({
-            "type": "Syntax Error",
+            "type": "Python Syntax Error",
             "message": str(e),
             "line": e.lineno
         })
@@ -26,5 +32,63 @@ def detect_python_errors(code: str):
             "message": "Avoid using '== False'.",
             "line": None
         })
+
+    return errors
+
+
+# C++ Detection
+def detect_cpp_errors(code: str):
+
+    errors = []
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".cpp") as temp:
+
+        temp.write(code.encode())
+
+        temp_file = temp.name
+
+    result = subprocess.run(
+        ["g++", temp_file],
+        capture_output=True,
+        text=True
+    )
+
+    if result.stderr:
+        errors.append({
+            "type": "C++ Compilation Error",
+            "message": result.stderr,
+            "line": None
+        })
+
+    os.remove(temp_file)
+
+    return errors
+
+
+# Java Detection
+def detect_java_errors(code: str):
+
+    errors = []
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".java") as temp:
+
+        temp.write(code.encode())
+
+        temp_file = temp.name
+
+    result = subprocess.run(
+        ["javac", temp_file],
+        capture_output=True,
+        text=True
+    )
+
+    if result.stderr:
+        errors.append({
+            "type": "Java Compilation Error",
+            "message": result.stderr,
+            "line": None
+        })
+
+    os.remove(temp_file)
 
     return errors
